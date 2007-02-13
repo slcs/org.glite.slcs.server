bfac015366b86cadb7c9fd53d8254ce4e956b6f8
Valery Tschopp <valery.tschopp@switch.ch>
2007-02-13 13:24:28 +0000

    create DN with a list of attribute

--- src/org/glite/slcs/dn/DNBuilder.java
+++ src/org/glite/slcs/dn/DNBuilder.java
@@ -1,5 +1,5 @@
 /*
- * $Id: DNBuilder.java,v 1.1 2006/10/27 12:11:24 vtschopp Exp $
+ * $Id: DNBuilder.java,v 1.2 2007/02/13 13:24:28 vtschopp Exp $
  * 
  * Created on Aug 4, 2006 by tschopp
  *
@@ -7,6 +7,7 @@
  */
 package org.glite.slcs.dn;
 
+import java.util.List;
 import java.util.Map;
 
 import org.glite.slcs.SLCSException;
@@ -17,7 +18,7 @@ import org.glite.slcs.SLCSServerComponent;
  * name-value to construct a unique personal certificate DN.
  * 
  * @author Valery Tschopp <tschopp@switch.ch>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public interface DNBuilder extends SLCSServerComponent {
 
@@ -32,5 +33,17 @@ public interface DNBuilder extends SLCSServerComponent {
      *             map.
      */
     public String createDN(Map attributes) throws SLCSException;
+
+    /**
+     * Returns the DN based on the List of Attributes.
+     * 
+     * @param attributes
+     *            The List of Attributes.
+     * @return The constructed DN.
+     * @throws SLCSException
+     *             if it is not possible to create a DN based on the attributes.
+     * @see org.glite.slcs.Attribute
+     */
+    public String createDN(List attributes) throws SLCSException;
 
 }
