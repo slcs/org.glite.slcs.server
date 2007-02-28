a49c75c0a4f827551d2f0942436c8a8406c8d362
Valery Tschopp <valery.tschopp@switch.ch>
2007-02-28 15:42:16 +0000

    SQL code to create the SLCS database

---
+++ sql/create-slcs-db.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE slcs;
+
+USE slcs;
+DROP TABLE IF EXISTS AuditEvent;
+CREATE TABLE AuditEvent (
+	eventId INTEGER NOT NULL AUTO_INCREMENT,
+	# event 
+	eventType INTEGER NOT NULL,
+	eventLevel INTEGER NOT NULL,
+	eventDate TIMESTAMP NOT NULL CURRENT_TIMESTAMP,
+	eventMessage VARCHAR(255) NOT NULL,
+	# user attributes
+	attributeUniqueId VARCHAR(255),
+	attributeSurname VARCHAR(255),
+	attributeGivenName VARCHAR(255),
+	attributeEmail VARCHAR(255),
+	attributeHomeOrganization VARCHAR(255),
+	attributeHomeOrganizationType VARCHAR(255),
+	attributeAffiliation VARCHAR(255),
+	# client
+	remoteAddress VARCHAR(64),
+	userAgent VARCHAR(255),
+	PRIMARY KEY (eventId)
+);
+
