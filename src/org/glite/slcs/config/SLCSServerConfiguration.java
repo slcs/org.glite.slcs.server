69d1a7cf3b60b9141f19faca8fe76b67994bb9ce
Valery Tschopp <valery.tschopp@switch.ch>
2007-03-19 13:14:13 +0000

    uses new AttributeDefinitions factory and engine.

--- src/org/glite/slcs/config/SLCSServerConfiguration.java
+++ src/org/glite/slcs/config/SLCSServerConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * $Id: SLCSServerConfiguration.java,v 1.6 2007/02/28 11:55:52 vtschopp Exp $
+ * $Id: SLCSServerConfiguration.java,v 1.7 2007/03/19 13:14:13 vtschopp Exp $
  * 
  * Created on Jul 28, 2006 by tschopp
  *
@@ -7,7 +7,6 @@
  */
 package org.glite.slcs.config;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.ServletContext;
@@ -15,8 +14,11 @@ import javax.servlet.ServletContext;
 import org.apache.commons.configuration.FileConfiguration;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.glite.slcs.AttributeDefinition;
 import org.glite.slcs.SLCSConfigurationException;
+import org.glite.slcs.SLCSException;
+import org.glite.slcs.attribute.AttributeDefinition;
+import org.glite.slcs.attribute.AttributeDefinitions;
+import org.glite.slcs.attribute.AttributeDefinitionsFactory;
 
 /**
  * SLCSServerConfiguration is the singleton instance of the SLCS server
@@ -24,19 +26,19 @@ import org.glite.slcs.SLCSConfigurationException;
  * in the <code>web.xml</code> file.
  * 
  * <pre>
- *             &lt;web-app id=&quot;SLCS&quot; version=&quot;2.4&quot;&gt;
- *             &lt;display-name&gt;SLCS&lt;/display-name&gt;
- *             &lt;!-- webapps context parameters --&gt;
- *             &lt;context-param&gt;
- *                  &lt;!-- MANDATORY SLCSServerConfigurationFile: absolute filename or file in classpath --&gt;
- *                  &lt;param-name&gt;SLCSServerConfigurationFile&lt;/param-name&gt;
- *                  &lt;param-value&gt;/etc/glite/slcs.xml&lt;/param-value&gt;
- *             &lt;/context-param&gt;
- *             ...
+ *  &lt;web-app id=&quot;SLCS&quot; version=&quot;2.4&quot;&gt;
+ *  &lt;display-name&gt;SLCS&lt;/display-name&gt;
+ *  &lt;!-- webapps context parameters --&gt;
+ *     &lt;context-param&gt;
+ *        &lt;!-- MANDATORY SLCSServerConfigurationFile: absolute filename or file in classpath --&gt;
+ *        &lt;param-name&gt;SLCSServerConfigurationFile&lt;/param-name&gt;
+ *        &lt;param-value&gt;/etc/glite/slcs.xml&lt;/param-value&gt;
+ *     &lt;/context-param&gt;
+ *     ...
  * </pre>
  * 
  * @author Valery Tschopp <tschopp@switch.ch>
- * @version $Revision: 1.6 $
+ * @version $Revision: 1.7 $
  */
 public class SLCSServerConfiguration extends SLCSConfiguration {
 
@@ -54,11 +56,6 @@ public class SLCSServerConfiguration extends SLCSConfiguration {
     static private String DEFAULT_CONFIGURATION_FILE = "slcs.xml";
 
     /**
-     * Key prefix for the attributes configuration in the configuration file
-     */
-    static public String ATTRIBUTESCONFIGURATION_PREFIX = "AttributesConfiguration";
-
-    /**
      * Key prefix for a server components configuration in the configuration
      * file
      */
@@ -70,24 +67,9 @@ public class SLCSServerConfiguration extends SLCSConfiguration {
     static private SLCSServerConfiguration SINGLETON = null;
 
     /**
-     * List of required attribute names
-     */
-    private List requiredAttributeNames_ = null;
-
-    /**
-     * List of defined attribute names
-     */
-    private List definedAttributeNames_ = null;
-
-    /**
-     * List of attribute defintions (only name and displayName are set)
-     */
-    private List attributeDefinitions_ = null;
-
-    /**
      * Helper for attributes
      */
-    private AttributeDefintionsHelper attributeDefintionsHelper_ = null;
+    private AttributeDefinitions attributeDefinitions_ = null;
 
     /**
      * Initialize the singleton instance of the SLCSServerConfiguration.
@@ -160,82 +142,47 @@ public class SLCSServerConfiguration extends SLCSConfiguration {
     protected SLCSServerConfiguration(String filename)
             throws SLCSConfigurationException {
         super();
-        LOG.debug("filename: " + filename);
+        LOG.info("XMLFilename=" + filename);
         FileConfiguration configuration = loadConfiguration(filename);
         // setFileConfiguration call checkConfiguration...
         setFileConfiguration(configuration);
 
-        initAttributesLists();
+        // load the attribute definitions
+        createAttributeDefinitions();
     }
 
-    private void initAttributesLists() throws SLCSConfigurationException {
-        // create the attribute names lists
-        requiredAttributeNames_ = new ArrayList();
-        definedAttributeNames_ = new ArrayList();
-        attributeDefinitions_ = new ArrayList();
+    private void createAttributeDefinitions() throws SLCSConfigurationException {
 
-        // populate both list
-        List attributeNames = getList(ATTRIBUTESCONFIGURATION_PREFIX
-                + ".Attribute[@name]");
-        int nAttributes = attributeNames.size();
-        for (int i = 0; i < nAttributes; i++) {
-            String name = (String) attributeNames.get(i);
-            // get required element, never throw exception (safe==false)
-            String prefix = ATTRIBUTESCONFIGURATION_PREFIX + ".Attribute(" + i
-                    + ")";
-            String required = getString(prefix + "[@required]", false);
-            if (required != null && required.equals("true")) {
-                // add to the required attribute names list
-                requiredAttributeNames_.add(name);
-            }
-
-            // add to the valid attribute names list
-            definedAttributeNames_.add(name);
-
-            // create a new attribute definition
-            String displayName = getString(prefix + "[@displayName]", false);
-            AttributeDefinition attributeDef = new AttributeDefinition(name, displayName);
-            if (required != null && required.equals("true")) {
-                attributeDef.setRequired(true);
-            }
-            // add in the attribute's definition list
-            attributeDefinitions_.add(attributeDef);
-
+        // read filename from config and getInstance
+        String definitionsFile = getString("AttributeDefinitions[@filename]");
+        try {
+            attributeDefinitions_ = AttributeDefinitionsFactory.getInstance(definitionsFile);
+        } catch (SLCSException e) {
+            LOG.error(e);
+            throw new SLCSConfigurationException("Failed to create AttributeDefinitions: "
+                    + definitionsFile, e);
         }
-        // create attributeDefinitionHelper
-        attributeDefintionsHelper_ = new AttributeDefintionsHelper(attributeDefinitions_);
-
-        LOG.info("AttributeDefinitions=" + attributeDefinitions_);
-        LOG.info("DefinedAttributeNames=" + definedAttributeNames_);
-        LOG.info("RequiredAttributeNames=" + requiredAttributeNames_);
     }
 
     /**
      * @return
      */
     public List getRequiredAttributeNames() {
-        return requiredAttributeNames_;
-    }
-
-    /**
-     * @return List of defined attribute names
-     */
-    public List getDefinedAttributeNames() {
-        return definedAttributeNames_;
+        return attributeDefinitions_.getRequiredAttributeNames();
     }
 
     /**
      * @return The list of {@link AttributeDefinition}s
      */
-    public List getAttributeDefinitions() {
-        return attributeDefinitions_;
+    public List getAttributeDefinitionsList() {
+        return attributeDefinitions_.getAttributeDefinitionsList();
     }
 
     /**
-     * @return The {@link AttributeDefintionsHelper}
+     * @return The {@link AttributeDefinitions} object
      */
-    public AttributeDefintionsHelper getAttributeDefintionsHelper() {
-        return attributeDefintionsHelper_;
+    public AttributeDefinitions getAttributeDefinitions() {
+        return attributeDefinitions_;
     }
 
     /*
@@ -245,13 +192,13 @@ public class SLCSServerConfiguration extends SLCSConfiguration {
      */
     protected void checkConfiguration() throws SLCSConfigurationException {
         String filename = getFilename();
-        // Shibboleth attributes definitions
-        if (!contains(ATTRIBUTESCONFIGURATION_PREFIX + ".Attribute[@name]")) {
+        // Shibboleth attributes definitions XML file
+        if (!contains("AttributeDefinitions[@filename]")) {
             LOG.error("SLCSServerConfiguration(" + filename + "): no "
-                    + ATTRIBUTESCONFIGURATION_PREFIX + ".Attribute defined");
+                    + "AttributeDefinitions[@filename] defined");
             throw new SLCSConfigurationException("Element(s) "
-                    + ATTRIBUTESCONFIGURATION_PREFIX
-                    + ".Attribute not defined in " + filename);
+                    + "AttributeDefinitions[@filename] not defined in "
+                    + filename);
         }
         // DNBuilder
         if (!contains(COMPONENTSCONFIGURATION_PREFIX
