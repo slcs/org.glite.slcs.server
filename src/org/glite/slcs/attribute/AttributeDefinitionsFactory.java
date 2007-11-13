743ca59310f70fdc9437f6b9280a63abb20c59b5
Valery Tschopp <valery.tschopp@switch.ch>
2007-11-13 14:33:37 +0000

    initialize the factory from the servlet context parameter AttributeDefinitionsFile

--- src/org/glite/slcs/attribute/AttributeDefinitionsFactory.java
+++ src/org/glite/slcs/attribute/AttributeDefinitionsFactory.java
@@ -1,5 +1,5 @@
 /*
- * $Id: AttributeDefinitionsFactory.java,v 1.4 2007/11/01 14:35:11 vtschopp Exp $
+ * $Id: AttributeDefinitionsFactory.java,v 1.5 2007/11/13 14:33:37 vtschopp Exp $
  *
  * Copyright (c) Members of the EGEE Collaboration. 2004.
  * See http://eu-egee.org/partners/ for details on the copyright holders.
@@ -7,11 +7,19 @@
  */
 package org.glite.slcs.attribute;
 
+import javax.servlet.ServletContext;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.glite.slcs.SLCSConfigurationException;
 import org.glite.slcs.SLCSException;
 
 public class AttributeDefinitionsFactory {
+
+    /**
+     * Parameter name in the servlet context or in the web.xml file
+     */
+    public static String ATTRIBUTE_DEFINITIONS_FILE_KEY = "AttributeDefinitionsFile";
 
     /** Singleton */
     static private AttributeDefinitions SINGLETON = null;
@@ -20,10 +28,36 @@ public class AttributeDefinitionsFactory {
     static private Log LOG = LogFactory.getLog(AttributeDefinitionsFactory.class);
 
     /**
+     * Initializes the factory using the servlet context parameter
+     * <code>AttributeDefintionsFile</code>.
+     * 
+     * @param context
+     *            The servlet context.
+     * @throws SLCSException
+     *             If the servlet context parameter is missing or if an error
+     *             occurs.
+     */
+    static public synchronized void initialize(ServletContext context)
+            throws SLCSException {
+        LOG.debug("initialize AttributeDefintionsFactory(ServletContext)");
+        String filename = context.getInitParameter(ATTRIBUTE_DEFINITIONS_FILE_KEY);
+        if (filename == null) {
+            LOG.error("Parameter " + ATTRIBUTE_DEFINITIONS_FILE_KEY
+                    + " not found in the servlet context");
+            throw new SLCSConfigurationException("Parameter "
+                    + ATTRIBUTE_DEFINITIONS_FILE_KEY
+                    + " not found in the servlet context");
+        }
+        initialize(filename);
+    }
+
+    /**
      * Initializes the factory with the given attribute definitions XML file.
      * 
      * @param filename
+     *            The attribute definitions XML absolute filename.
      * @throws SLCSException
+     *             If an error occurs.
      */
     static public synchronized void initialize(String filename)
             throws SLCSException {
