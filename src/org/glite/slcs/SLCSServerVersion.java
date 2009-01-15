2c803533fbe04af484f95f66400acf32ce529030
Valery Tschopp <valery.tschopp@switch.ch>
2009-01-15 12:31:13 +0000

    copyright updated

--- src/org/glite/slcs/SLCSServerVersion.java
+++ src/org/glite/slcs/SLCSServerVersion.java
@@ -1,11 +1,19 @@
 /*
- * $Id: SLCSServerVersion.java,v 1.22 2009/01/15 12:30:47 vtschopp Exp $
- * 
- * Created on May 5, 2006 by tschopp
+ * Copyright (c) 2007-2009. Members of the EGEE Collaboration.
  *
- * Copyright (c) Members of the EGEE Collaboration. 2004.
- * See http://eu-egee.org/partners/ for details on the copyright holders.
- * For license conditions see the license file or http://eu-egee.org/license.html
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * $Id: SLCSServerVersion.java,v 1.23 2009/01/15 12:31:13 vtschopp Exp $
  */
 package org.glite.slcs;
 
@@ -13,7 +21,7 @@ package org.glite.slcs;
  * SLCSServerVersion and Copyright constants.
  *
  * @author Valery Tschopp &lt;tschopp@switch.ch&gt;
- * @version $Revision: 1.22 $
+ * @version $Revision: 1.23 $
  */
 public class SLCSServerVersion {
 
